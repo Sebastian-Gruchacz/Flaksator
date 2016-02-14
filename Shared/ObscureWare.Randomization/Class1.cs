60dc6add98737e0f723b4c6c3735e34e771a8a80
Sebastian Gruchacz <sebastian@gruchacz.com>
2016-02-15 00:55:21 +0100

    Randomzier & revolver.

--- Shared/ObscureWare.Randomization/Class1.cs
+++ Shared/ObscureWare.Randomization/Class1.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ObscureWare.Randomization
 {
-    public class Class1
+    public interface IRandomizer
     {
+
+
+        
+    }
+
+
+    public class RandomizerState<T> where T : IRandomizer
+    {
+        private readonly byte[] _state;
+
+        private RandomizerState(byte[] stateArray)
+        {
+            _state = stateArray;
+        }
+
+        public T Restore()
+        {
+            var binaryFormatter = new BinaryFormatter();
+            using (var temp = new MemoryStream(_state))
+            {
+                return (T)binaryFormatter.Deserialize(temp);
+            }
+        }
+
+        public static RandomizerState<T> Save(T randomizer)
+        {
+            if (randomizer == null) throw new ArgumentNullException(nameof(randomizer));
+
+            var binaryFormatter = new BinaryFormatter();
+            using (var temp = new MemoryStream())
+            {
+                binaryFormatter.Serialize(temp, randomizer);
+                return new RandomizerState<T>(temp.ToArray());
+            }
+        }
     }
 }
