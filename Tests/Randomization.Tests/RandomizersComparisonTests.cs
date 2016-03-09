405f017789f5116f658129b67a27d43ff5a6eb3f
Sebastian Gruchacz <lo@gruchacz.com>
2016-03-09 18:29:21 +0100

    Testing randomizers speed and distribution.

--- Tests/Randomization.Tests/RandomizersComparisonTests.cs
+++ Tests/Randomization.Tests/RandomizersComparisonTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using MersenneTwister;
 using NUnit.Framework;
 using SharpDevs.Randomization;
@@ -36,7 +38,69 @@ namespace Randomization.Tests
 
                 Console.WriteLine();
             }
+        }
 
+        [Test]
+        public void CompareDistributionOfMersennes()
+        {
+            Random rnd = new Random();
+            int baseSeed = rnd.Next();
+
+            var m1 = new MT19937();
+            m1.init_genrand((ulong)baseSeed);
+            var m2 = new RandomMT((ulong)baseSeed);
+            var m3 = new SharpDevs.Randomization.MersenneTwister(baseSeed);
+            var m4 = new Random(baseSeed);
+            var maxRange = 100;
+            var iterations = 100000;
+
+            var dist1 = CalculateDistribution(() => m1.RandomRange(0, maxRange - 1), maxRange, iterations);
+            var dist2 = CalculateDistribution(() => m2.RandomRange(0, maxRange - 1), maxRange, iterations);
+            var dist3 = CalculateDistribution(() => m3.GetNext(0, maxRange), maxRange, iterations);
+            var dist4 = CalculateDistribution(() => m4.Next(0, maxRange), maxRange, iterations);
+
+            var distribution1 = AnalyzeDistribution(dist1, iterations);
+            var distribution2 = AnalyzeDistribution(dist2, iterations);
+            var distribution3 = AnalyzeDistribution(dist3, iterations);
+            var distribution4 = AnalyzeDistribution(dist4, iterations);
+
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}",
+                distribution1, distribution2, distribution3, distribution4);
+            var expectedFill = (decimal) iterations/maxRange;
+            Console.WriteLine("Przy oczekiwanym napełnieniu: " + expectedFill);
+            Console.WriteLine("Co daje procentowo:");
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}",
+                distribution1 / expectedFill * 100,
+                distribution2 / expectedFill * 100,
+                distribution3 / expectedFill * 100,
+                distribution4 / expectedFill * 100);
+        }
+
+        private decimal AnalyzeDistribution(int[] distribution, int iterations)
+        {
+            int expectedScores = distribution.Length;
+            decimal expectedScore = (decimal)iterations/ expectedScores;
+            var deviation = distribution.Average(i => Math.Abs(i - expectedScore));
+            return deviation;
+        }
+
+        private int[] CalculateDistribution(Func<int> generator, int maxRange, int iterations)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            int[] resultScore = new int[maxRange];
+
+            for (int i = 0; i < iterations; i++)
+            {
+                int score = generator.Invoke();
+                resultScore[score]++;
+            }
+
+            sw.Stop();
+            Console.WriteLine(@"Czas wykonania: " + sw.ElapsedMilliseconds + @"ms");
+
+            return resultScore;
         }
     }
+    
 }
